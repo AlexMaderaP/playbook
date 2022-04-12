b66d795e724a724b8243ec7400a0c0a712deecb1
AlexMaderaP <alejandro.maderap@gmail.com>
2022-04-12 12:58:17 -0500

    Agregando ejercicios semana 1

---
+++ weekly_mission_1/example5/main.js
@@ -0,0 +1,7 @@
+const logger = require("./logger");
+
+// Ya se puede usar directamente el objeto instanciado en el módulo logger
+//logger.log("This is an informational message");
+
+const customLogger = new logger.constructor("CUSTOM");
+customLogger.log("This is an informational message");
